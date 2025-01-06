2c041d15d1b881521c1fce6e9a5c0b06fb492ee7
huutri12 <tri_1951220112@dau.edu.vn>
2025-01-06 22:33:21 +0700

    validation_with_joi

---
+++ src/validations/boardValidation.js
@@ -0,0 +1,37 @@
+import Joi from 'joi'
+import { StatusCodes } from 'http-status-codes'
+
+const createNew = async (req, res, next) => {
+  /**
+   * Note: Mặc định chúng ta không cần phải custom message ở phía BE làm gì vì để cho Front-end tự validate
+   và custom message phía FE cho đẹp.
+   * Back-end chỉ cần validate Đảm bảo dữ liệu chuẩn xác, và trả về message mặc định từ thư viện là được.
+   * Quan trọng: Việc Validate dữ liệu BẮT BUỘC phải có ở phía Back-end vì đây là điểm cuối để lưu trữ dữ
+    liệu vào Database.
+   * Và thông thường trong thực tế, điều tốt nhất cho hệ thống là hãy luôn validate dữ liệu ở cả Back-end
+    và Front-end nhé.
+   */
+  const correctCondition = Joi.object({
+    title: Joi.string().required().min(3).max(50).trim().strict(),
+    description: Joi.string().required().min(3).max(256).trim().strict()
+  })
+
+  try {
+    console.log('req.body', req.body)
+
+    // Chỉ định abortEarly: false để trường hợp có nhiều lỗi validation thì trả về tất cả lỗi
+    await correctCondition.validateAsync(req.body, { abortEarly: false })
+    // next()
+    res.status(StatusCodes.CREATED).json({ message: 'POST from Validation: API create new board' })
+  } catch (error) {
+    console.log(error)
+    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
+      errors: new Error(error).message
+    })
+  }
+
+}
+
+export const boardValidation = {
+  createNew
+}
