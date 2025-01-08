120234e534ca512749f07a4b15336532b37f8719
huutri12 <tri_1951220112@dau.edu.vn>
2025-01-08 23:53:55 +0700

    error_handling

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,17 +1,14 @@
 import { StatusCodes } from 'http-status-codes'
 
-
 const createNew = async (req, res, next) => {
   try {
-    console.log('req.body: ', req.body)
-    res.status(StatusCodes.CREATED).json({ message: 'POST from Controller: API create new board' })
-  } catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      errors: error.message
-    })
-  }
-}
+    // console.log('req.body: ', req.body)
 
+    // Điều hướng dữ liệu sang tầng Service
+
+    res.status(StatusCodes.CREATED).json({ message: 'POST from Controller: API create new board' })
+  } catch (error) {next(error)}
+}
 
 export const boardController = {
   createNew
--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -1,5 +1,6 @@
 import Joi from 'joi'
 import { StatusCodes } from 'http-status-codes'
+import ApiError from '~/utils/ApiError'
 
 const createNew = async (req, res, next) => {
   /**
@@ -23,9 +24,7 @@ const createNew = async (req, res, next) => {
     // Validate du lieu xong xuoi hop le thi cho request di tiep sang controller
     next()
   } catch (error) {
-    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
-      errors: new Error(error).message
-    })
+    next(new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, new Error(error).message))
   }
 
 }
