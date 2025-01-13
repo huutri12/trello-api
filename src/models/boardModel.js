0d408a8c1b734e76a1a13852fc36b78ac2a7479a
huutri12 <tri_1951220112@dau.edu.vn>
2025-01-13 22:10:23 +0700

    api_get_details_of_board

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -33,9 +33,14 @@ const createNew = async (data) => {
 
 const findOneById = async (id) => {
   try {
-    const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({
-      _id: new ObjectId(id)
-    })
+    const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({ _id: new ObjectId(id) })
+    return result
+  } catch (error) {throw new Error(error)}
+}
+
+const getDetails = async (id) => {
+  try {
+    const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({ _id: new ObjectId(id) })
     return result
   } catch (error) {throw new Error(error)}
 }
@@ -44,5 +49,6 @@ export const boardModel = {
   BOARD_COLLECTION_NAME,
   BOARD_COLLECTION_SCHEMA,
   createNew,
-  findOneById
+  findOneById,
+  getDetails
 }
