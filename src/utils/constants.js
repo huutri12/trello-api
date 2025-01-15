f73993f81fb7b14fb161327b4fffc5349952791e
huutri12 <tri_1951220112@dau.edu.vn>
2025-01-15 21:51:06 +0700

    config_cors

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,6 @@
-/**
 
- */
+// Những domain đươc phép truy câp tới tài nguyên của Server
+export const WHITELIST_DOMAINS = [
+  'http://localhost:5173'
+  // ... ví du sau này sẽ deloy lên domain chính thức
+]
